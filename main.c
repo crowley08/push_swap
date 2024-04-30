b34299f4811a4c2fbfe12a40eefc9298bdd5a7d5
Saotra Fahasoavana Andrianome <saandria@c1r7s7.42antananarivo.mg>
2024-04-30 14:32:01 +0300

    push_swap

---
+++ main.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saandria <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 09:40:17 by saandria          #+#    #+#             */
+/*   Updated: 2024/04/30 13:56:39 by saandria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include <stdio.h>
+
+int	main(int argc, char *argv[])
+{
+	t_stack	*a;
+	t_stack	*b;
+	char	**args;
+	int		i;
+
+	i = 0;
+	if (argc == 1)
+		return (0);
+	if (argc == 2)
+	{
+		args = ft_split(argv[1], ' ');
+		i = 0;
+	}
+	else if (argc > 2)
+	{
+		args = argv;
+		i = 1;
+	}
+	b = NULL;
+	a = create_stack(args, i);
+	printf("size a:%d\nsize b:%d\n" , stack_size(a), stack_size(b));
+	//rra(&a);
+	//sa(&a);
+	pb(&a, &b);
+	pb(&a, &b);
+	pb(&a, &b);
+	printf("size a:%d\nsize b:%d\n" , stack_size(a), stack_size(b));
+	write(1, "a\n", 2);
+	while (a)
+	{
+		printf("%d\n", a->value);
+		a = a->next;
+	}
+	write(1, "b\n", 2);
+	while (b)
+	{
+		printf("%d\n", b->value);
+		b = b->next;
+	}
+}
---
+++ push_swap.h
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: saandria <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 08:59:24 by saandria          #+#    #+#             */
+/*   Updated: 2024/04/30 11:42:39 by saandria         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+
+typedef struct	s_stack
+{
+	int				value;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+}					t_stack;
+
+/*************************************swap*************************************/
+void	swap(t_stack **stack);
+void	sa(t_stack **a);
+void	sb(t_stack **b);
+void	ss(t_stack **a, t_stack **b);
+/*************************************rotate*************************************/
+void	rotate(t_stack **stack);
+void	ra(t_stack **a);
+void	rb(t_stack **b);
+void	rr(t_stack **a, t_stack **b);
+/*************************************rotate*************************************/
+void	rev_rotate(t_stack **stack);
+void	rra(t_stack **a);
+void	rrb(t_stack **b);
+void	rrr(t_stack **a, t_stack **b);
+/*************************************rotate*************************************/
+void	push(t_stack **src, t_stack **dest);
+void	pa(t_stack **b, t_stack **a);
+void	pb(t_stack **a, t_stack **b);
+/*************************************stack_utils*************************************/
+t_stack	*stack_new(int value);
+t_stack	*find_last(t_stack *stack);
+void	add_stack(t_stack **root, t_stack *to_add);
+t_stack	*create_stack(char **args, int i);
+int		stack_size(t_stack *stack);
+
+#endif
